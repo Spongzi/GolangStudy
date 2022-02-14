415bdb983d91d412a3a06983b2f6483d699973f9
unknown <lxlandsx@outlook.com>
2022-02-14 11:15:18 +0800

    2022年2月14日上午复习内容

--- operation/main.go
+++ operation/main.go
@@ -1,1 +1,8 @@
-package operation
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1 << 2的结果", 1<<2)
+	fmt.Println("1 >> 2的结果", 1>>2)
+}
--- variable/main.go
+++ variable/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+// 全局变量
+var m = 100
+
+func main() {
+	n := 10
+	m := 200 // 此时声明变量为m
+	fmt.Println(n, m)
 }
